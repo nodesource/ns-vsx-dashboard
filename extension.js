652e2fba5d9654a08f457ebc4b151c589ab29a09
Thorsten Lorenz <thlorenz@gmx.de>
2018-11-05 17:47:05 -0500

    code: detecting died agents and introduced logger abstraction

--- extension.js
+++ extension.js
@@ -5,14 +5,16 @@ const AgentManager = require('./lib/agent-manager')
 const { initAgentStatusbar } = require('./lib/agent-statusbar')
 const { initAgentErrors } = require('./lib/agent-errors')
 
-function activate(context) {
-  console.log('"ns-vsx-dashboard" is now active!')
+const { logInfo } = require('./lib/logger')('main')
 
+function activate(context) {
   const connector = new AgentConnector()
   const agentManager = new AgentManager(connector)
 
   initAgentStatusbar(agentManager)
   initAgentErrors({ agentManager })
+
+  logInfo('"ns-vsx-dashboard" is now active!')
 }
 
 function deactivate() { }
