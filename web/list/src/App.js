0cad90b0d487deb82c68162e881bcc9e2560de5a
Thorsten Lorenz <thlorenz@gmx.de>
2018-11-08 16:17:38 -0500

    webview: list rendering updating agents (simulated ATM)

--- web/list/src/App.js
+++ web/list/src/App.js
@@ -1,5 +1,6 @@
 import agentsJSON from '../agents.json'
 import ta from 'time-ago'
+import prettyBytes from 'pretty-bytes'
 const agents = new Map(agentsJSON)
 const MAX_ID_LEN = 15
 const MAX_MAIN_LEN = 45
@@ -27,41 +28,80 @@ function getId(id) {
   return `${s.slice(0, MAX_ID_LEN)}...`
 }
 
-function agentsArray(agents) {
-  const arr = []
-  for (const [ id, agent ] of agents) {
-    const info = agent.info
-    // Don't show agents for which we still are missing info
-    if (info == null) continue
-    const { nodeEnv, processStart, main } = info
-    const x = {
-        id: getId(id)
-      , name: nameAgent(agent.info)
-      , nodeEnv
-      , processStart: ta.ago(processStart)
-      , main: getMain(main)
-       // TODO: get the below from metrics
-      , heapUsage: 100
-      , cpuUsage: 45
-      , activeRequests: 27
-      , activeHandles: 68
-    }
-    arr.push(x)
+function processAgentInfo(id, info) {
+  const { nodeEnv, processStart, main } = info
+  return {
+      id: getId(id)
+    , name: nameAgent(info)
+    , nodeEnv
+    , processStart: ta.ago(processStart)
+    , main: getMain(main)
   }
-  return arr
+}
+
+function processAgentMetrics(metrics) {
+  const {
+      heapUsed
+    , activeRequests
+    , activeHandles
+    , cpu
+  } = metrics
+
+  return {
+      heapUsed: prettyBytes(heapUsed)
+    , activeRequests
+    , activeHandles
+    , cpu: Math.round(cpu)
+  }
 }
 
 class App {
-  constructor(data = {}) {
-    this._data = data
-    setInterval(() => {
-      this._data.agents[0].heapUsage++
-    }, 200)
-    setTimeout(() => this._data.agents.pop(), 2000)
+  constructor() {
+    // maintaining map for quick access of all agents and array for agents
+    // that should be visisble in the UI
+    this._data = { agentsMap: new Map(), agents: [] }
+  }
+
+  addInfo(id, info) {
+    if (info == null) return
+    const agent = this._getOrCreate(id)
+    const wasShown = !!agent.show
+    agent.id = id
+    agent.show = agent.metrics != null
+    agent.info = Object.assign(agent.info || {}, processAgentInfo(id, info))
+    this._maybeAddToVisibles(wasShown, agent)
+  }
+
+  addMetrics(id, metrics) {
+    if (metrics == null) return
+    const agent = this._getOrCreate(id)
+    const wasShown = !!agent.show
+    agent.metrics = Object.assign(agent.metrics || {}, processAgentMetrics(metrics))
+    agent.show = agent.info != null
+    this._maybeAddToVisibles(wasShown, agent)
+  }
+
+  removeAgent(id) {
+    this._data.agentsMap.delete(id)
+    const idx = this._data.agents.findIndex(x => x.id === id)
+    if (idx >= 0) this._data.agents.splice(idx, 1)
   }
 
   log(...args) {
     console.log(...args)
+  }
+
+  _getOrCreate(id) {
+    const { agentsMap } = this._data
+    if (!agentsMap.has(id)) {
+      agentsMap.set(id, { info: null, metrics: null, shown: false })
+    }
+    return agentsMap.get(id)
+  }
+
+  _maybeAddToVisibles(wasShown, agent) {
+    if (wasShown || !agent.show) return
+    this._data.agents.push(agent)
   }
 
   data() {
@@ -69,13 +109,27 @@ class App {
   }
 }
 
-const agentsArr = agentsArray(agents)
-
-const initialData = { agents: agentsArr }
-const app = new App(initialData)
-
+export const app = new App()
 export const methods = {
   log: app.log.bind(app)
 }
-
 export const data = app.data.bind(app)
+
+// Test
+
+function init() {
+  for (const [ id, agent ] of agents) {
+    app.addInfo(id, agent.info)
+  }
+}
+
+function simulateMetrics() {
+  for (const [ id, agent ] of agents) {
+    if (agent.collectedMetrics == null) continue
+    app.addMetrics(id, agent.collectedMetrics.shift())
+  }
+}
+
+init()
+simulateMetrics()
+setInterval(simulateMetrics, 500)
--- web/list/src/App.vue
+++ web/list/src/App.vue
@@ -8,39 +8,39 @@
         >
           <v-list-tile
             v-for="agent in agents"
-            :key="agent.title"
+            :key="agent.id"
             @click="() => log('clicked')"
           >
             <v-list-tile-content>
               <v-list-tile-title>
-               🕵 {{ agent.name }} ({{ agent.id }})
+               🕵 {{ agent.info.name }} ({{ agent.info.id }})
               </v-list-tile-title>
-              <v-list-tile-sub-title v-html="agent.main"></v-list-tile-sub-title>
+              <v-list-tile-sub-title v-html="agent.info.main"></v-list-tile-sub-title>
               <v-divider></v-divider>
               <v-list-tile-sub-title class="blue--text">
                 <p>
-									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 18 18"><path d="M13 9H9v4h4V9zm2-7h-1V1h-2v1H6V1H4v1H3c-.55 0-1 .45-1 1v12c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H4V6h10v8z"/></svg>								
-                  <span class="ml-1 mr-1" v-html="agent.processStart" />
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 18 18"><path d="M13 9H9v4h4V9zm2-7h-1V1h-2v1H6V1H4v1H3c-.55 0-1 .45-1 1v12c0 .55.45 1 1 1h12c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H4V6h10v8z"/></svg>
+                  <span class="ml-1 mr-1" v-html="agent.info.processStart" />
                 </p>
 
-                <p>
+                <p style="min-width: 100px;">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"/></svg>
-                  <span class="ml-1 mr-1" v-html="agent.heapUsage" />
+                  <span style="font-family: Inconsolate Monospace;" class="ml-1 mr-1" v-html="agent.metrics.heapUsed" />
                 </p>
 
-                <p>
+                <p style="min-width: 80px;">
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15 9H9v6h6V9zm-2 4h-2v-2h2v2zm8-2V9h-2V7c0-1.1-.9-2-2-2h-2V3h-2v2h-2V3H9v2H7c-1.1 0-2 .9-2 2v2H3v2h2v2H3v2h2v2c0 1.1.9 2 2 2h2v2h2v-2h2v2h2v-2h2c1.1 0 2-.9 2-2v-2h2v-2h-2v-2h2zm-4 6H7V7h10v10z"/></svg>
-                  <span class="ml-1 mr-1" v-html="agent.cpuUsage" />
+                  <span class="ml-1 mr-1">{{ agent.metrics.cpu }}%</span>
                 </p>
 
                 <p>
                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.01 14H2v2h7.01v3L13 15l-3.99-4v3zm5.98-1v-3H22V8h-7.01V5L11 9l3.99 4z"/></svg>
-                  <span class="ml-1 mr-1" v-html="agent.activeRequests" />
+                  <span class="ml-1 mr-1" v-html="agent.metrics.activeRequests" />
                 </p>
 
                 <p>
-									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>								
-                  <span class="ml-1 mr-1" v-html="agent.activeHandles" />
+									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z"/></svg>
+                  <span class="ml-1 mr-1" v-html="agent.metrics.activeHandles" />
                 </p>
               </v-list-tile-sub-title>
             </v-list-tile-content>
@@ -53,5 +53,5 @@
 
 <script>
 import { methods, data } from './App.js'
-export default { methods, data }  
+export default { methods, data }
 </script>
