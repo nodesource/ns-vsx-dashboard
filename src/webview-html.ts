2fbc793b6207ca04cd48d7a7d22a65517679f92f
Thorsten Lorenz <thlorenz@gmx.de>
2018-12-04 14:24:54 -0600

    ts: completing ts setup

--- src/webview-html.ts
+++ src/webview-html.ts
@@ -2,7 +2,9 @@ import { readFileSync } from 'fs'
 import { join } from 'path'
 
 export default function webviewHtml(appName: string): string {
-  const webRoot = join(__dirname, '..', 'web')
+  // Reach out of generated directory as webviews are currently generated
+  // inside the original web/ dir
+  const webRoot = join(__dirname, '..', '..', 'web')
   const webAppRoot = join(webRoot, appName)
   const webviewPath = join(webAppRoot, 'webview.html')
   return readFileSync(webviewPath, 'utf8')
